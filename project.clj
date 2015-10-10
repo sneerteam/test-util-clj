9ded014fff7b54cde3f20c8cb5f1c2e1ec6ea38f
Klaus Wuestefeld <klauswuestefeld@gmail.com>
2015-10-10 20:45:08 -0300

    Initial commit

---
+++ project.clj
@@ -0,0 +1,9 @@
+(defproject sneerteam/test-util-clj "0.1.0"
+  :description "Common functions for testing Clojure"
+  :url "https://github.com/sneerteam/test-util-clj"
+  :license {:name "BSD"
+            :url "http://opensource.org/licenses/BSD-3-Clause"}
+  :dependencies [[org.clojure/clojure "1.7.0"]
+                 [org.clojure/core.async "0.1.346.0-17112a-alpha"]
+                 [sneerteam/commons-clj "0.1.0"]]
+  :repositories [["clojars" { :sign-releases false}]])
---
+++ src/sneer/test_util.clj
@@ -0,0 +1,99 @@
+(ns sneer.test-util
+  (:require
+    [sneer.commons :refer [nvl loop-trace]]
+    [clojure.core.async :refer [alt!! timeout filter> >!! <!! close! chan]])
+  (:import [java.io File]
+           [java.lang.reflect Field]))
+
+(defn ->clj [o]
+  (when (some? o)
+    (into {} (map (fn [^Field field] [(keyword (.getName field)) (.get field o)])
+                  (.getFields (class o))))))
+
+(defn tmp-file []
+  (doto
+    (File/createTempFile "test-" ".tmp")
+    (.delete)))
+
+(defn tmp-folder []
+  (let [^File folder (tmp-file)]
+    (.mkdir folder)
+    folder))
+
+(def ^:private default-timeout 400)
+
+(defn >!!?
+  ([ch v]
+    (>!!? ch v default-timeout))
+  ([ch v timeout-millis]
+    (alt!!
+      (timeout timeout-millis) (throw (RuntimeException. (str "TIMEOUT putting: " v)))
+      [[ch v]] :>!!?-return)))
+
+(defn <!!?
+  ([ch]
+    (<!!? ch default-timeout))
+  ([ch timeout-millis]
+    (alt!!
+      (timeout timeout-millis) :timeout
+      ch ([v] v))))
+
+(defn ->predicate [expected]
+  (if (fn? expected) expected #(= % expected)))
+
+(defn try? [pred current]
+  (try
+    [(pred current) nil]
+    (catch Exception e
+      [false e])))
+
+(defn not-ok [msg last-value ^Throwable last-exception]
+  (do
+    (println msg last-value)
+    (when last-exception
+      (.printStackTrace last-exception))
+    false))
+
+(defn <wait-trace! [ch expected]
+  (let [expected (nvl expected :nil)
+        pred (->predicate expected)]
+    (loop-trace [last-value "<none>"
+                 last-exception nil]
+      (let [current (<!!? ch)]
+        (cond
+          (= current :timeout) (not-ok "TIMEOUT. Last value emitted:" last-value last-exception)
+          (nil? current)       (not-ok "COMPLETED/CLOSED. Last value emitted:" last-value last-exception)
+          :else (let [[success exception] (try? pred current)]
+                  (if success
+                    true
+                    (recur current (or exception last-exception)))))))))
+
+(defn compromised
+  ([ch] (compromised ch 0.7))
+  ([ch failure-rate]
+    (filter> (fn [_] (> (rand) failure-rate)) ch)))
+
+(defn compromised-if [unreliable ch]
+  (if unreliable
+    (compromised ch)
+    ch))
+
+(defn pst [fn]
+  (try (fn)
+       (catch Exception e (.printStackTrace e))))
+
+(defn <emits [expected]
+  (fn [ch]
+    (<wait-trace! ch expected)))
+
+(defn closes [ch]
+  (loop-trace [last-value "<none>"]
+              (if-some [current (<!!? ch)]
+                (if (= current :timeout)
+                  (do
+                    (println "TIMEOUT. Last value emitted:" last-value)
+                    false)
+                  (recur current))
+                true)))
+
+; (do (require 'midje.repl) (midje.repl/autotest))
